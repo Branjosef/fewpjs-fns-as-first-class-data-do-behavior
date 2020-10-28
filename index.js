5586410f71be6289eac41fbcf5ed0c3b89a74822
Brandon Fonnemann <branjosef@gmail.com>
2020-10-27 20:21:22 -0500

    commit tree 5e266cae4a8808c15c0cb22e278c8a122766d6e1

--- index.js
+++ index.js
@@ -9,5 +9,22 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+function greet(time) {
+let hour = parseInt(time)
+  if (hour < 12) {
+    return `Good Morning`;
+  } else if (hour >= 12 && hour <= 17){
+    return `Good Afternoon`;
+  } else {
+    return `Good Evening`;
+  }
+};
+
+function displayMessage (message) {
+document.getElementById('greeting').innerText = message;
+};
+
+
+
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
