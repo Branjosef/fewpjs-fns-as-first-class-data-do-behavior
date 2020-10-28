7f44bb6851c7ca2fa65d652834aebab42320ef90
Brandon Fonnemann <branjosef@gmail.com>
2020-10-27 20:22:18 -0500

    Done.

--- index.js
+++ index.js
@@ -9,5 +9,22 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
+function greet(time) {
+let hour = parseInt(time)
+  if (hour < 12) {
+    return `Good Morning`;
+  } else if (hour >= 12 && hour <= 17){
+    return `Good Afternoon`;
+  } else {
+    return `Good Evening`;
+  }
+};
+
+function displayMessage (message) {
+document.getElementById('greeting').innerText = message;
+};
+
+// need to remember that when using getElementById, I do not need the hashtag bc it is already getting by ID
+
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
